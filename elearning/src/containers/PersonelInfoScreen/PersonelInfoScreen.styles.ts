0221adb3a37e1de96e3f63555ddcedab99e28524
mann579 <mann.singh579@gmail.com>
2021-04-05 19:24:57 +0200

    refactor personal info screen

--- elearning/src/containers/PersonelInfoScreen/PersonelInfoScreen.styles.ts
+++ elearning/src/containers/PersonelInfoScreen/PersonelInfoScreen.styles.ts
@@ -1,6 +1,36 @@
 import styled from 'styled-components/native';
+import { StyleSheet, SafeAreaView, TextInput, Text, TouchableHighlight } from 'react-native';
 import { PersonelInfoScreenProps } from './PersonelInfoScreen.interface';
 
-export const PersonelInfoScreen = styled.View<Pick<PersonelInfoScreenProps, 'myProp'>>`
-  /* Add styles here */
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  justify-content: center;
+  margin: 0px 16px;
+`;
+
+export const TitleText = styled(Text)`
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const Input = styled(TextInput)`
+  height: 40px;
+  margin-top: 50px;
+  border-bottom-color: #b0adac;
+  border-bottom-width: 0.5px;
+  border-color: white;
+`;
+
+export const SaveButton = styled(TouchableHighlight)`
+  height: 50px;
+  border-radius: 10px;
+  background-color: #1b8ffa;
+  margin-top: 100px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SaveText = styled(Text)`
+  color: #FFFFFF;
 `;
--- elearning/src/containers/PersonelInfoScreen/PersonelInfoScreen.tsx
+++ elearning/src/containers/PersonelInfoScreen/PersonelInfoScreen.tsx
@@ -1,66 +1,29 @@
 import React, { FC } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text,TextInput, StyleSheet,Button,Alert,SafeAreaView, TouchableHighlight,TouchableOpacity } from 'react-native';
-import { PersonelInfoScreen as PersonelInfoScreenComponent } from './PersonelInfoScreen.styles';
+import { View, Text, Alert } from 'react-native';
+import { Container, Input, TitleText, SaveButton, SaveText } from './PersonelInfoScreen.styles';
 import { PersonelInfoScreenProps } from './PersonelInfoScreen.interface';
 
 export const PersonelInfoScreen: FC<PersonelInfoScreenProps> = () => {
   const navigation = useNavigation();
   return (
    
-    <SafeAreaView style={styles.container}>
+    <Container>
    <View>
-      <Text style={styles.titleText} >
-      Personel info
-     
-      </Text>
-      <TextInput
-        style={styles.input}
+      <TitleText>Personel info</TitleText>
+      <Input
         placeholder="Full Name" 
       />    
-      <TextInput
-      style={styles.input}
-      placeholder="Occocupation" 
-    />
-     <TextInput
-      style={styles.input}
+      <Input
+        placeholder="Occocupation" 
+      />
+     <Input
       placeholder="Language" 
     />
-        <TouchableHighlight  style={styles.touchHighlight}>
-            <Button onPress={() => Alert.alert('Simple Button pressed')}            
-            title="SAVE"
-            color="#FFFFFF"
-            accessibilityLabel="Learn more about this button"
-            /> 
-          </TouchableHighlight> 
+      <SaveButton onPress={() => Alert.alert('Simple Button pressed')}>
+        <SaveText>SAVE</SaveText> 
+      </SaveButton> 
     </View>
-    </SafeAreaView>
+    </Container>
   );
-
 };
-
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    marginTop: 50,
-    borderBottomColor: '#b0adac',
-    borderBottomWidth: 0.5,
-    borderColor: 'white', 
-  },
-  titleText: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  }, 
-  touchHighlight: {
-    height: 50,
-    borderRadius:10,
-    backgroundColor : '#1b8ffa',
-    marginTop :100,
-  },
-});
