9d2452d04570d4833fb82cc061ae7b79218ddfa0
sukhi0077 <sukhi0077@gmail.com>
2021-04-14 17:41:25 +0200

    myCourse Page

--- elearning/src/containers/MyCoursesScreen/MyCoursesScreen.styles.ts
+++ elearning/src/containers/MyCoursesScreen/MyCoursesScreen.styles.ts
@@ -1,6 +1,95 @@
 import styled from 'styled-components/native';
-import { MyCoursesScreenProps } from './MyCoursesScreen.interface';
+import { SafeAreaView, Text ,View, StyleSheet,TouchableOpacity} from 'react-native';
 
-export const MyCoursesScreen = styled.View<Pick<MyCoursesScreenProps, 'myProp'>>`
-  /* Add styles here */
+
+
+export const FirstName = styled(Text)`
+  font-size: 30px;
+  margin-top: 50px;
+  color: white;
 `;
+export const LastName = styled(Text)`
+  font-size: 30px;
+  color: white;
+`;
+export const OccupationText = styled(Text)`
+  margin-top: 10px;
+  color: white;
+`;
+
+export const EmailText = styled(Text)`
+  font-size: 00px;
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-bottom: 60px;
+  color: black;
+`;
+
+export const Container = styled(SafeAreaView)`
+  flex: 0;
+  margin: 0px;
+  margin-top: 0px;
+`;
+
+
+export const TitleText = styled(Text)`
+  color: white;
+  align-self: flex-start;
+  font-size: 34px;
+  font-weight: bold;
+  margin-top: 100px;
+  margin-left: 10px;
+`;
+export const TitleContainer = styled(View)`
+  height: 140px;
+  background-color: #2b2b2b;
+  flex-direction: row;
+`;
+
+
+export const FavButtonContainer = styled(TouchableOpacity)`
+  height: 30px;
+  border-radius: 10px;
+  margin: 30px 16px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FavoriteButton = styled(Text)`
+  color: #1b8ffa;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0px 15px;
+`;
+
+export const PageText = styled(Text)`
+  color: #1b8ffa;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0px 15px;
+`;
+
+export const styles = StyleSheet.create({
+  pagerView: {
+    height: 200,
+    width: '100%'
+  },
+  container: {
+    flex: 1,
+    padding: 0,
+  },
+
+});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 0,
+//   },
+//   pagerView: {
+//     height: 200,
+//     width: '100%'
+//   },
+// });
--- elearning/src/containers/MyCoursesScreen/MyCoursesScreen.tsx
+++ elearning/src/containers/MyCoursesScreen/MyCoursesScreen.tsx
@@ -1,7 +1,68 @@
-import React, { FC } from 'react';
-import { MyCoursesScreen as MyCoursesScreenComponent } from './MyCoursesScreen.styles';
-import { MyCoursesScreenProps } from './MyCoursesScreen.interface';
+import React, { useRef } from 'react';
+import {
+  TitleText,
+  styles,
+  FavButtonContainer,
+  FavoriteButton,
+  PageText,
+} from './MyCoursesScreen.styles';
+import { View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import PagerView from 'react-native-pager-view';
 
-export const MyCoursesScreen: FC<MyCoursesScreenProps> = ({ children }) => (
-  <MyCoursesScreenComponent>{children}</MyCoursesScreenComponent>
-);
+export const MyCoursesScreen = () => {
+  const pagerViewRef = useRef<PagerView>(null);
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: 'column',
+        },
+      ]}
+    >
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        <TitleText>My Course</TitleText>
+        <View
+          style={[
+            styles.container,
+            {
+              flexDirection: 'row',
+            },
+          ]}
+        ></View>
+      </View>
+      <View style={{ flex: 4, backgroundColor: 'white' }}>
+      <View style={{flexDirection: 'row'}}>
+        <FavButtonContainer onPress={() => {
+            pagerViewRef?.current?.setPage(0);
+          }}>
+          <FavoriteButton>All</FavoriteButton>
+        </FavButtonContainer >
+        <FavButtonContainer onPress={() => {
+            pagerViewRef?.current?.setPage(1);
+          }}>
+          <FavoriteButton>Studying</FavoriteButton>
+        </FavButtonContainer>
+        <FavButtonContainer onPress={() => {
+            pagerViewRef?.current?.setPage(2);
+          }}>
+          <FavoriteButton>Subscribed</FavoriteButton>
+        </FavButtonContainer>
+        </View>
+        <PagerView style={styles.pagerView} initialPage={0} ref={pagerViewRef}>
+        <View>
+          <PageText>List ALL page</PageText>
+        </View>
+        <View key="1">
+          <PageText>List Currently Studying</PageText>
+        </View>
+        <View key="2">
+          <PageText>List Subscribed Page</PageText>
+        </View>
+      </PagerView>
+        </View>   
+    </View>
+  );
+};
+
