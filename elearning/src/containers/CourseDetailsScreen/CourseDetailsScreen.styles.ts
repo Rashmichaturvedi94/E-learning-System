8ef29c892e15a6225ac02e3facb0729c4b0d88e4
sukhi0077 <sukhi0077@gmail.com>
2021-05-03 20:12:15 +0200

    formatting and style

--- elearning/src/containers/CourseDetailsScreen/CourseDetailsScreen.styles.ts
+++ elearning/src/containers/CourseDetailsScreen/CourseDetailsScreen.styles.ts
@@ -1,31 +1,51 @@
 import styled from 'styled-components/native';
 import { CourseDetailsScreenProps } from './CourseDetailsScreen.interface';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Text } from 'react-native-elements';
+import { Image } from 'react-native-elements/dist/image/Image';
+
+export const TouchPlay = styled(TouchableOpacity)`
+  height: 50px;
+  width: 50px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListItemContainer = styled(View)`
+  margin-left: 20;
+  flex: 1;
+  align-items: flex-start;
+`;
+
+export const ListTitle = styled(Text)`
+  font-weight: bold;
+`;
+export const ListImage = styled(Image)`
+  width: 40px;
+  height: 40px;
+`;
+
+export const ListView = styled(View)`
+  flex: 3;
+  background-color: white;
+`;
+export const ListItemView = styled(View)`
+  flex-direction: row;
+  flex: 1;
+`;
+
+export const Container = styled(View)`
+  flex-direction: column;
+  flex: 5;
+  padding: 5px;
+`;
 
 export const styles = StyleSheet.create({
   pagerView: {
     height: 200,
     width: '100%',
   },
-  container: {
-    flex: 1,
-    padding: 0,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 30,
-    marginVertical: 20,
-    marginHorizontal: 20,
-  },
-  listItem: {
-    backgroundColor: '#FFF',
-    marginVertical: 20,
-    marginHorizontal: 20,
-    flexDirection: 'row'
-  },
   mainImage: {
-    //padding:50,
-    //marginTop: 50,
     resizeMode: 'stretch',
     width: '100%',
     height: '100%',
--- elearning/src/containers/CourseDetailsScreen/CourseDetailsScreen.tsx
+++ elearning/src/containers/CourseDetailsScreen/CourseDetailsScreen.tsx
@@ -1,60 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { styles } from './CourseDetailsScreen.styles';
+import {
+  ListImage,
+  ListItemContainer,
+  ListTitle,
+  ListView,
+  styles,
+  TouchPlay,
+  ListItemView,
+  Container,
+} from './CourseDetailsScreen.styles';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import { Icon, ListItem } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 
 function Item({ item }) {
   return (
-    <View style={styles.listItem}>
-      <Image source={{ uri: item.url }} style={{ width: 40, height: 40 }} />
-
-      <View style={{ alignItems: 'flex-start', flex: 1, marginStart: 10 }}>
-        <Text style={{ fontWeight: 'bold' }}>{item.title}</Text>
+    <ListItemView>
+      <ListImage source={{ uri: item.url }}></ListImage>
+      <ListItemContainer>
+        <ListTitle>{item.title}</ListTitle>
         <Text>{item.desc}</Text>
-      </View>
-
-      <TouchableOpacity
-        style={{
-          height: 50,
-          width: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Icon name="video" />
-      </TouchableOpacity>
-    </View>
+      </ListItemContainer>
+      <TouchPlay>
+        <Icon name="pause"></Icon>
+      </TouchPlay>
+    </ListItemView>
   );
 }
 
 export const CourseDetailsScreen = () => {
-  const DATA = [
-    {
-      name: 'Introduction',
-      email: 'miyah.myles@gmail.com',
-      position: 'OOPS, Poly',
-      photo:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
-    },
-    {
-      name: 'June Cha',
-      email: 'june.cha@gmail.com',
-      position: 'Sales Manager',
-      photo: 'https://randomuser.me/api/portraits/women/44.jpg',
-    },
-    {
-      name: 'Iida Niskanen',
-      email: 'iida.niskanen@gmail.com',
-      position: 'Sales Manager',
-      photo: 'https://randomuser.me/api/portraits/women/68.jpg',
-    },
-  ];
   const [lessonStore, setLessonStore] = useState();
 
   useEffect(() => {
     const lessonStore1 = firestore()
-      .collection('course/c1/lesson').orderBy("title")
+      .collection('course/c1/lesson')
+      .orderBy('title')
       .get()
       .then((query) => {
         var arr1 = [];
@@ -66,29 +46,24 @@ export const CourseDetailsScreen = () => {
       });
   });
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          flexDirection: 'column',
-        },
-      ]}
-    >
+    <Container>
       <View style={{ flex: 2 }}>
-      <Image style={styles.mainImage}
-        source={{
-          uri: 'https://miro.medium.com/max/8642/1*iIXOmGDzrtTJmdwbn7cGMw.png',
-        }}
-      />
+        <Image
+          style={styles.mainImage}
+          source={{
+            uri:
+              'https://miro.medium.com/max/8642/1*iIXOmGDzrtTJmdwbn7cGMw.png',
+          }}
+        />
       </View>
-      <View style={{ flex: 3, backgroundColor: 'white' }}>
+      <ListView>
         <FlatList
-          style={{ flex: 1, marginTop:100 }}
+          style={{ flex: 1, marginTop: 100 }}
           data={lessonStore}
           renderItem={({ item }) => <Item item={item} />}
           keyExtractor={(item) => item.title}
         />
-      </View>
-    </View>
+      </ListView>
+    </Container>
   );
 };
