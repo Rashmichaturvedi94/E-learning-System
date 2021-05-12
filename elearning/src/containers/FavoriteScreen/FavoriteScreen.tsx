694f452009378fde998ccd0b6f593659546a4788
sukhi0077 <sukhi0077@gmail.com>
2021-05-12 16:28:02 +0200

    Update FavoriteScreen.tsx

--- elearning/src/containers/FavoriteScreen/FavoriteScreen.tsx
+++ elearning/src/containers/FavoriteScreen/FavoriteScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, SectionList, StatusBar } from 'react-native';
 import PagerView from 'react-native-pager-view';
 import {
   TitleText,
@@ -8,8 +8,45 @@ import {
   FavoriteButton,
   PageText,
 } from './FavoriteScreen.styles';
+import firestore from '@react-native-firebase/firestore';
+import { CourseList } from 'components/CourseList';
 
 export const FavoriteScreen = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    StatusBar.setBarStyle('light-content', true);
+  });
+  useEffect(() => {
+    firestore()
+      .collection('course')
+      .get()
+      .then((query) => {
+        const arr1 = [];
+        query.forEach((doc) => {
+          arr1.push(doc.data());
+        });
+        const formatted = [
+          {
+            //title: 'My List',
+            data: [
+              {
+                courses: arr1,
+              },
+            ],
+          },
+          // {
+          //   title: 'Mobile',
+          //   data: [
+          //     {
+          //       courses: arr1,
+          //     },
+          //   ],
+          // },
+        ];
+        setCourses(formatted);
+      });
+  }, []);
+
   const pagerViewRef = useRef<PagerView>(null);
   return (
     <View
@@ -24,34 +61,56 @@ export const FavoriteScreen = () => {
         <TitleText>Favorite</TitleText>
       </View>
       <View style={{ flex: 4, backgroundColor: 'white' }}>
-         <View style={{flexDirection: 'row'}}>
-        <FavButtonContainer onPress={() => {
-             pagerViewRef?.current?.setPage(0);
-           }}>
-           <FavoriteButton>Favorite</FavoriteButton>
-         </FavButtonContainer >
-         <FavButtonContainer onPress={() => {
-             pagerViewRef?.current?.setPage(1);
-           }}>
-           <FavoriteButton>Badges</FavoriteButton>
-         </FavButtonContainer>
-         <FavButtonContainer onPress={() => {
-             pagerViewRef?.current?.setPage(2);
-           }}>
-           <FavoriteButton>Scores</FavoriteButton>
-         </FavButtonContainer>
-       </View>
-       <PagerView style={styles.pagerView} initialPage={0} ref={pagerViewRef}>
-         <View>
-           <PageText>Favorite page</PageText>
-       </View>
-         <View key="1">
-           <PageText>Badge page</PageText>
-         </View>
-         <View key="2">
-           <PageText>Score page</PageText>
-         </View>
-       </PagerView>
+        <View style={{ flexDirection: 'row' }}>
+          <FavButtonContainer
+            onPress={() => {
+              pagerViewRef?.current?.setPage(0);
+            }}
+          >
+            <FavoriteButton>Favorite</FavoriteButton>
+          </FavButtonContainer>
+          <FavButtonContainer
+            onPress={() => {
+              pagerViewRef?.current?.setPage(1);
+            }}
+          >
+            <FavoriteButton>Badges</FavoriteButton>
+          </FavButtonContainer>
+          <FavButtonContainer
+            onPress={() => {
+              pagerViewRef?.current?.setPage(2);
+            }}
+          >
+            <FavoriteButton>Scores</FavoriteButton>
+          </FavButtonContainer>
+        </View>
+        <PagerView style={styles.pagerView} initialPage={0} ref={pagerViewRef}>
+          <View>
+            <SectionList
+              sections={courses}
+              renderItem={({ section: { data } }) => {
+                return (
+                  <CourseList
+                    data={data[0].courses}
+                    onItemPress={(item) => {
+                      // handleCoursePress(item);
+                    }}
+                  />
+                );
+              }}
+              keyExtractor={(item, index) => index.toString()}
+              renderSectionHeader={({ section: { title } }) => (
+                <Text style={styles.header}>{title}</Text>
+              )}
+            />
+          </View>
+          <View key="1">
+            <PageText>Badge page</PageText>
+          </View>
+          <View key="2">
+            <PageText>Score page</PageText>
+          </View>
+        </PagerView>
       </View>
     </View>
   );
