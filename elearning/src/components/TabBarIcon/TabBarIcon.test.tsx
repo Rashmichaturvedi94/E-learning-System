a9c483adeb7dc40a989e0929179af694e2f82103
mann579 <mann.singh579@gmail.com>
2021-05-29 13:57:50 +0200

    tab bar test fix

--- elearning/src/components/TabBarIcon/TabBarIcon.test.tsx
+++ elearning/src/components/TabBarIcon/TabBarIcon.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { HomeIcon } from 'components/Icon';
 import { TabBarIcon } from './TabBarIcon';
 
 describe('components/TabBarIcon', () => {
   it('should render', () => {
-    const component = render(<TabBarIcon />);
+    const component = render(<TabBarIcon Icon={HomeIcon} />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
